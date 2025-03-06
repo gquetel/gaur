11e98d2560226951eaf152da1a0c235bbfd76ab9
gquetel <gregor.quetel@gmail.com>
2025-03-06 17:17:28 +0100

    feat: shell.nix exports CUSTOM_INTERPRETER_PATH variable

--- shell.nix
+++ shell.nix
@@ -1,15 +1,42 @@
-let 
-    inputs = import ./npins; 
-    pkgs =  import inputs.nixpkgs { };
-    pygaur-package = import ./pygaur/default.nix;
-in 
-
-pkgs.mkShell {
-  buildInputs = [
-    pygaur-package 
+let
+  inputs = import ./npins;
+  pkgs = import inputs.nixpkgs { };
+  pygaur = pkgs.callPackage ./pygaur/default.nix {
+    inherit (pkgs.python311Packages) 
+    buildPythonPackage
+    setuptools
+    pandas
+    gensim
+    numpy
+    sentence-transformers
+    transformers
+    torch
+    ;
+  };
+  pythonEnv = pkgs.python311.withPackages (ps: [
+    pygaur
+    # ps.jupyter
+    # ps.notebook
+    # ps.ipython
+    ps.transformers
+    ps.torch
+    ps.pandas
+    ps.gensim
+    ps.numpy
+    ps.sentence-transformers
+  ]);
+in
+pkgs.mkShell rec {
+  packages = [
+    pythonEnv
     pkgs.flex
     pkgs.bison
     pkgs.libgcc
     pkgs.gnumake
   ];
+
+  catchConflicts = false;
+  shellHook = ''
+    export CUSTOM_INTERPRETER_PATH="${pythonEnv}/bin/python"
+  '';
 }
